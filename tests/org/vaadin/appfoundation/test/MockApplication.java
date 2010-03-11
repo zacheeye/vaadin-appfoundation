80f4f20f1c3c7a7359a849d88d89f1d85c4eab01
Kim <Kim@localhost>
2010-03-11 20:57:42 +0200

    Added a mock application

---
+++ tests/org/vaadin/appfoundation/test/MockApplication.java
@@ -0,0 +1,21 @@
+package org.vaadin.appfoundation.test;
+
+import com.vaadin.Application;
+
+/**
+ * Mock implementation of a Vaadin application
+ * 
+ * @author Kim
+ * 
+ */
+public class MockApplication extends Application {
+
+    private static final long serialVersionUID = -7592972194969208126L;
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
