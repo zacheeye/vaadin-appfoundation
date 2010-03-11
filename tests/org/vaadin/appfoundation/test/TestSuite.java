bfa3177f0a68ef1c36ea28293e6dbd3ecb2706d7
Kim <Kim@localhost>
2010-03-11 21:01:40 +0200

    Added LangTest to the test suite

--- tests/org/vaadin/appfoundation/test/TestSuite.java
+++ tests/org/vaadin/appfoundation/test/TestSuite.java
@@ -4,9 +4,10 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.vaadin.appfoundation.test.authorization.AuthorizationUtilTest;
 import org.vaadin.appfoundation.test.i18n.FillXmlTest;
 import org.vaadin.appfoundation.test.i18n.InternationalizationServletTest;
+import org.vaadin.appfoundation.test.i18n.LangTest;
 
 @SuiteClasses( { AuthorizationUtilTest.class, FillXmlTest.class,
-        InternationalizationServletTest.class })
+        InternationalizationServletTest.class, LangTest.class })
 public class TestSuite {
 
 }
