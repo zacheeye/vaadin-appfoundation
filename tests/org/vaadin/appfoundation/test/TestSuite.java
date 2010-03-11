ca8810fcf372a5f7e4c5ee7b0fd296dba465dbea
Kim <Kim@localhost>
2010-03-11 21:14:34 +0200

    Added test for the TranslationUtil

--- tests/org/vaadin/appfoundation/test/TestSuite.java
+++ tests/org/vaadin/appfoundation/test/TestSuite.java
@@ -5,9 +5,11 @@ import org.vaadin.appfoundation.test.authorization.AuthorizationUtilTest;
 import org.vaadin.appfoundation.test.i18n.FillXmlTest;
 import org.vaadin.appfoundation.test.i18n.InternationalizationServletTest;
 import org.vaadin.appfoundation.test.i18n.LangTest;
+import org.vaadin.appfoundation.test.i18n.TranslationUtilTest;
 
 @SuiteClasses( { AuthorizationUtilTest.class, FillXmlTest.class,
-        InternationalizationServletTest.class, LangTest.class })
+        InternationalizationServletTest.class, LangTest.class,
+        TranslationUtilTest.class })
 public class TestSuite {
 
 }
