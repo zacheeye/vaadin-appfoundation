ab736be2829ef4453d3a9a397d3d7a36a7987667
kim <kim@localhost>
2010-03-12 14:57:00 +0200

    Added new tests to test suite

--- tests/org/vaadin/appfoundation/test/TestSuite.java
+++ tests/org/vaadin/appfoundation/test/TestSuite.java
@@ -1,5 +1,7 @@
 package org.vaadin.appfoundation.test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.vaadin.appfoundation.test.authorization.AuthorizationUtilTest;
 import org.vaadin.appfoundation.test.i18n.FillXmlTest;
@@ -7,12 +9,14 @@ import org.vaadin.appfoundation.test.i18n.InternationalizationServletTest;
 import org.vaadin.appfoundation.test.i18n.LangTest;
 import org.vaadin.appfoundation.test.i18n.TranslationUtilTest;
 import org.vaadin.appfoundation.test.view.DefaultViewFactoryTest;
+import org.vaadin.appfoundation.test.view.ViewHandlerTest;
 import org.vaadin.appfoundation.test.view.ViewItemTest;
 
+@RunWith(Suite.class)
 @SuiteClasses( { AuthorizationUtilTest.class, FillXmlTest.class,
         InternationalizationServletTest.class, LangTest.class,
         TranslationUtilTest.class, ViewItemTest.class,
-        DefaultViewFactoryTest.class })
+        DefaultViewFactoryTest.class, ViewHandlerTest.class })
 public class TestSuite {
 
 }
