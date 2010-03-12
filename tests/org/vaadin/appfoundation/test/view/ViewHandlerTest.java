b09b8534c1eed0b1041a47dacf1000355e2cf237
kim <kim@localhost>
2010-03-12 13:53:56 +0200

    Added a bunch of tests for the ViewHandler. Mainly methods related to adding views

--- tests/org/vaadin/appfoundation/test/view/ViewHandlerTest.java
+++ tests/org/vaadin/appfoundation/test/view/ViewHandlerTest.java
@@ -8,6 +8,10 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.vaadin.appfoundation.test.MockApplication;
+import org.vaadin.appfoundation.view.AbstractView;
+import org.vaadin.appfoundation.view.DefaultViewFactory;
+import org.vaadin.appfoundation.view.ViewContainer;
+import org.vaadin.appfoundation.view.ViewFactory;
 import org.vaadin.appfoundation.view.ViewHandler;
 import org.vaadin.appfoundation.view.ViewItem;
 
@@ -34,16 +38,96 @@ public class ViewHandlerTest {
     }
 
     @Test
-    public void addView() {
+    public void addViewNoParams() {
         // With no parameters
         Object id1 = ViewHandler.addView();
         assertNotNull(id1);
         assertNotNull(ViewHandler.getViewItem(id1));
+    }
 
+    @Test
+    public void addViewObjectParam() {
         // With a view id param
         String id2 = "id2";
         ViewHandler.addView(id2);
         ViewItem item = ViewHandler.getViewItem(id2);
+        assertNotNull(item);
+        assertEquals(id2, item.getViewId());
     }
 
+    @Test
+    public void addViewClassParam() {
+        // Add view with an id of a View class
+        ViewItem item2 = ViewHandler.addView(MockView.class);
+        assertEquals(MockView.class, item2.getViewClass());
+        assertTrue(item2.getFactory() instanceof DefaultViewFactory);
+    }
+
+    @Test
+    public void addViewExistingView() {
+        String id2 = "id2";
+        ViewHandler.addView(id2);
+
+        // Try adding a view with an existing id
+        assertNull(ViewHandler.addView(id2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addViewNullParam() {
+        ViewHandler.addView(null);
+    }
+
+    @Test
+    public void addViewWithParent() {
+        final ValueContainer value = new ValueContainer();
+
+        ViewContainer container = new ViewContainer() {
+            public void activate(AbstractView<?> view) {
+                value.setValue(view);
+            }
+        };
+
+        ViewItem item = ViewHandler.addView(MockView.class, container);
+
+        ViewHandler.activateView(MockView.class);
+        assertNotNull(value.getValue());
+        assertEquals(item.getView(), value.getValue());
+    }
+
+    @Test
+    public void setDefaultViewFactory() {
+        ViewFactory factory = new ViewFactory() {
+            public AbstractView<?> initView(Object viewId) {
+                return null;
+            }
+        };
+
+        // Make sure the new view factory is set
+        assertNull(ViewHandler.getDefaultViewFactory());
+        ViewHandler.setDefaultViewFactory(factory);
+        assertNotNull(ViewHandler.getDefaultViewFactory());
+        assertEquals(factory, ViewHandler.getDefaultViewFactory());
+
+        // Add new view and make sure it got our new default view factory as its
+        // factory
+        ViewItem item = ViewHandler.addView(MockView.class);
+        assertNotNull(item.getFactory());
+        assertEquals(factory, item.getFactory());
+    }
+
+    private class ValueContainer {
+
+        private Object value;
+
+        public ValueContainer() {
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+    }
 }
