07c43964ac81a6945a50ef4fcd063039282c1f63
Kim <Kim@localhost>
2010-03-26 07:10:15 +0200

    Added new methods for the role

--- src/org/vaadin/appfoundation/authorization/Role.java
+++ src/org/vaadin/appfoundation/authorization/Role.java
@@ -1,5 +1,7 @@
 package org.vaadin.appfoundation.authorization;
 
+import java.util.Set;
+
 /**
  * Interface for roles
  * 
@@ -14,5 +16,34 @@ public interface Role {
      * @return
      */
     public Object getIdentifier();
+
+    /**
+     * Roles are hierarchical, this method returns other roles that have been
+     * <b>directly</b> assigned to this role.
+     * 
+     * @return
+     */
+    public Set<Role> getRoles();
+
+    /**
+     * Set all the roles which are <b>directly</b> assigned to this role.
+     * 
+     * @param roles
+     */
+    public void setRoles(Set<Role> roles);
+
+    /**
+     * Assign this role another sub-role.
+     * 
+     * @param role
+     */
+    public void addRole(Role role);
+
+    /**
+     * Remove an assigned sub-role from this role.
+     * 
+     * @param role
+     */
+    public void removeRole(Role role);
 
 }
