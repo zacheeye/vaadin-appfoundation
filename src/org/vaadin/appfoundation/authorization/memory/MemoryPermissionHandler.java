83e444e10098b27ed95e1e9ddeb69ffd1bc098a1
kim <kim@localhost>
2010-03-26 16:28:34 +0200

    Implementation of a Memory based PermissionHandler

--- src/org/vaadin/appfoundation/authorization/memory/MemoryPermissionHandler.java
+++ src/org/vaadin/appfoundation/authorization/memory/MemoryPermissionHandler.java
@@ -14,67 +14,130 @@ import org.vaadin.appfoundation.authorization.Role;
 public class MemoryPermissionHandler implements PermissionHandler {
 
     /**
-     * {@inheritDoc}
+     * Contains the "allowed" permissions for those permission where an explicit
+     * action has been defined.
      */
-    public void allow(Role role, String action, Resource resource) {
-        // TODO Auto-generated method stub
+    private final PermissionMap allowed;
 
+    /**
+     * Contains the "denied" permissions for those permission where an explicit
+     * action has been defined.
+     */
+    private final PermissionMap denied;
+
+    /**
+     * Contains all global "allowed" permissions, these include permissions set
+     * with allowDefault and allowAll.
+     */
+    private final PermissionMap globalAllowed;
+
+    /**
+     * Contains all global "denied" permissions, these include permissions set
+     * with denyDefault and denyAll.
+     */
+    private final PermissionMap globalDenied;
+
+    public MemoryPermissionHandler() {
+        allowed = new PermissionMap();
+        denied = new PermissionMap();
+        globalAllowed = new PermissionMap();
+        globalDenied = new PermissionMap();
     }
 
     /**
      * {@inheritDoc}
      */
-    public void allow(Role role, Resource resource) {
-        // TODO Auto-generated method stub
+    public void allow(Role role, String action, Resource resource) {
+        checkArguments(role, resource);
 
+        if (denied.contains(role, action, resource)) {
+            denied.remove(role, action, resource);
+        }
+
+        allowed.put(role, action, resource);
     }
 
     /**
      * {@inheritDoc}
      */
     public void allowAll(Role role, Resource resource) {
-        // TODO Auto-generated method stub
+        checkArguments(role, resource);
 
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void allowDefault(Role role, Resource resource) {
-        // TODO Auto-generated method stub
-
+        denied.removeAll(role, resource);
+        globalDenied.removeAll(role, resource);
+        globalAllowed.put(role, "all", resource);
     }
 
     /**
      * {@inheritDoc}
      */
     public void deny(Role role, String action, Resource resource) {
-        // TODO Auto-generated method stub
+        checkArguments(role, resource);
 
+        if (allowed.contains(role, action, resource)) {
+            allowed.remove(role, action, resource);
+        }
+
+        denied.put(role, action, resource);
     }
 
     /**
      * {@inheritDoc}
      */
     public void denyAll(Role role, Resource resource) {
-        // TODO Auto-generated method stub
+        checkArguments(role, resource);
 
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void denyDefault(Role role, Resource resource) {
-        // TODO Auto-generated method stub
-
+        allowed.removeAll(role, resource);
+        globalAllowed.removeAll(role, resource);
+        globalDenied.put(role, "all", resource);
     }
 
     /**
      * {@inheritDoc}
      */
     public boolean hasAccess(Role role, String action, Resource resource) {
-        // TODO Auto-generated method stub
-        return false;
+        checkArguments(role, resource);
+
+        if (allowed.contains(role, action, resource)) {
+            return true;
+        }
+
+        if (denied.contains(role, action, resource)) {
+            return false;
+        }
+
+        if (globalAllowed.contains(role, "all", resource)) {
+            return true;
+        }
+
+        if (globalDenied.contains(role, "all", resource)) {
+            return false;
+        }
+
+        if (globalAllowed.hasPermissions(resource)
+                || allowed.hasPermissions(resource)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks that both the role and the resource is set.
+     * 
+     * @param role
+     * @param resource
+     * @throws IllegalArgumentException
+     *             Thrown if either role or resource is null
+     */
+    private void checkArguments(Role role, Resource resource) {
+        if (role == null) {
+            throw new IllegalArgumentException("Role may not be null");
+        }
+
+        if (resource == null) {
+            throw new IllegalArgumentException("Role may not be null");
+        }
     }
 
 }
