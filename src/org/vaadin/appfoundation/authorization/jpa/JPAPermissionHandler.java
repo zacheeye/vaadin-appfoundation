593eec3908a89ed865b0029fec873c54521828c9
kim <kim@localhost>
2010-03-26 14:39:55 +0200

    A stub for the JPA based implementation of the PermissionHandler

---
+++ src/org/vaadin/appfoundation/authorization/jpa/JPAPermissionHandler.java
@@ -0,0 +1,81 @@
+package org.vaadin.appfoundation.authorization.jpa;
+
+import org.vaadin.appfoundation.authorization.PermissionHandler;
+import org.vaadin.appfoundation.authorization.Resource;
+import org.vaadin.appfoundation.authorization.Role;
+
+/**
+ * JPA based implementation for the PermissionHandler interface. This class will
+ * communicate directly with the database, where it will store all defined
+ * permissions.
+ * 
+ * @author Kim
+ * 
+ */
+public class JPAPermissionHandler implements PermissionHandler {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void allow(Role role, String action, Resource resource) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void allow(Role role, Resource resource) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void allowAll(Role role, Resource resource) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void allowDefault(Role role, Resource resource) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void deny(Role role, String action, Resource resource) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void denyAll(Role role, Resource resource) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void denyDefault(Role role, Resource resource) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean hasAccess(Role role, String action, Resource resource) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
---
+++ src/org/vaadin/appfoundation/authorization/jpa/PermissionEntity.java
@@ -0,0 +1,178 @@
+package org.vaadin.appfoundation.authorization.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+
+import org.vaadin.appfoundation.authorization.Role;
+import org.vaadin.appfoundation.persistence.data.AbstractPojo;
+
+/**
+ * This entity class is used for persisting permissions used by the
+ * JPAPermissionHandler.
+ * 
+ * @author Kim
+ * 
+ */
+@Entity
+public class PermissionEntity extends AbstractPojo {
+
+    private static final long serialVersionUID = 3895345053504819128L;
+
+    @Enumerated(EnumType.STRING)
+    private PermissionType type;
+
+    @ManyToOne
+    private Role role;
+
+    private String resource;
+
+    private String action;
+
+    private boolean defaultPermission = false;
+
+    private boolean allPermissions = false;
+
+    /**
+     * Default constructor.
+     */
+    public PermissionEntity() {
+
+    }
+
+    /**
+     * Alternative constructor, sets the permission type upon initialization.
+     * 
+     * @param type
+     *            The permission's type
+     */
+    public PermissionEntity(PermissionType type) {
+        this.type = type;
+    }
+
+    /**
+     * Sets the type of the permission (allow/deny)
+     * 
+     * @param type
+     *            The permission's type
+     */
+    public void setType(PermissionType type) {
+        this.type = type;
+    }
+
+    /**
+     * Returns the type of the permission (allow/deny)
+     * 
+     * @return The permission's type
+     */
+    public PermissionType getType() {
+        return type;
+    }
+
+    /**
+     * Get the role for which this permission is being applied on.
+     * 
+     * @return The role for which the permission is being set
+     */
+    public Role getRole() {
+        return role;
+    }
+
+    /**
+     * Set the role for which this permission is being applied on.
+     * 
+     * @param role
+     *            The role for which the permission is being set
+     */
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    /**
+     * Get the resource's identifier for which this permission is being applied
+     * on.
+     * 
+     * @return The resource's identifier
+     */
+    public String getResource() {
+        return resource;
+    }
+
+    /**
+     * Set the resource's identifier for which this permission is being applied
+     * on.
+     * 
+     * @param resource
+     *            The resource's identifier
+     */
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    /**
+     * Get the action for which this permission is being applied on.
+     * 
+     * @return The action parameter for this permission
+     */
+    public String getAction() {
+        return action;
+    }
+
+    /**
+     * Set the action for which this permission is being applied on.
+     * 
+     * @param action
+     *            The action parameter for this permission
+     */
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    /**
+     * Is this permission the default permission for all actions for the given
+     * role in the given resource, if no other permissions are set for the
+     * action.
+     * 
+     * @return Returns true if this is the default permission, false if not
+     */
+    public boolean isDefaultPermission() {
+        return defaultPermission;
+    }
+
+    /**
+     * Defines if this permission the default permission for all actions for the
+     * given role in the given resource, if no other permissions are set for the
+     * action.
+     * 
+     * @param defaultPermission
+     *            True if this is the default permission, false if not
+     */
+    public void setDefaultPermission(boolean defaultPermission) {
+        this.defaultPermission = defaultPermission;
+    }
+
+    /**
+     * Should this permission be applied for all the actions in the given
+     * resource for the given role.
+     * 
+     * @return True if this permission is being applied for all action, false if
+     *         not
+     */
+    public boolean isAllPermissions() {
+        return allPermissions;
+    }
+
+    /**
+     * Defines if this permission should be applied for all the actions in the
+     * given resource for the given role.
+     * 
+     * @param allPermissions
+     *            True if this permission is being applied for all action, false
+     *            if not
+     */
+    public void setAllPermissions(boolean allPermissions) {
+        this.allPermissions = allPermissions;
+    }
+
+}
