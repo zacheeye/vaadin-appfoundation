42b5732d45ddf33973f241ab7a9882c79d4ab53a
kim <kim@localhost>
2010-04-01 16:54:39 +0300

    Added the Permissions helper class, tests included

---
+++ tests/org/vaadin/appfoundation/test/authorization/PermissionManagerMock.java
@@ -0,0 +1,45 @@
+package org.vaadin.appfoundation.test.authorization;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.vaadin.appfoundation.authorization.PermissionManager;
+import org.vaadin.appfoundation.authorization.Resource;
+import org.vaadin.appfoundation.authorization.Role;
+
+public class PermissionManagerMock implements PermissionManager {
+
+    private final List<String> invokedMethods = new ArrayList<String>();
+
+    public void allow(Role role, String action, Resource resource) {
+        invokedMethods.add("allow");
+    }
+
+    public void allowAll(Role role, Resource resource) {
+        invokedMethods.add("allowAll");
+    }
+
+    public void deny(Role role, String action, Resource resource) {
+        invokedMethods.add("deny");
+    }
+
+    public void denyAll(Role role, Resource resource) {
+        invokedMethods.add("denyAll");
+    }
+
+    public boolean hasAccess(Role role, String action, Resource resource) {
+        invokedMethods.add("hasAccessObject");
+        return false;
+    }
+
+    public boolean hasAccess(Set<Role> roles, String action, Resource resource) {
+        invokedMethods.add("hasAccessSet");
+        return false;
+    }
+
+    public boolean wasInvoked(String method) {
+        return invokedMethods.contains(method);
+    }
+
+}
