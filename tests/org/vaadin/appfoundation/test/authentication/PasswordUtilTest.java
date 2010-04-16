d26ef65796d297435cd2eac620b5f6e46e14b1ed
kim <kim@localhost>
2010-04-16 15:48:45 +0300

    Change properties API

--- tests/org/vaadin/appfoundation/test/authentication/PasswordUtilTest.java
+++ tests/org/vaadin/appfoundation/test/authentication/PasswordUtilTest.java
@@ -1,15 +1,29 @@
 package org.vaadin.appfoundation.test.authentication;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 
+import org.junit.After;
 import org.junit.Test;
 import org.vaadin.appfoundation.authentication.data.User;
 import org.vaadin.appfoundation.authentication.util.PasswordUtil;
 
 public class PasswordUtilTest {
+
+    @After
+    public void tearDown() throws SecurityException, NoSuchFieldException,
+            IllegalArgumentException, IllegalAccessException {
+        Field field = PasswordUtil.class.getDeclaredField("salt");
+        field.setAccessible(true);
+        field.set(null, null);
+
+        System.clearProperty("authentication.password.salt");
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void setPropertiesWithNull() {
@@ -34,6 +48,9 @@ public class PasswordUtilTest {
         Properties properties = new Properties();
         properties.setProperty("password.salt", "foobar");
         PasswordUtil.setProperties(properties);
+
+        properties.setProperty("password.salt", "foobar2");
+        PasswordUtil.setProperties(properties);
     }
 
     @Test
@@ -56,10 +73,33 @@ public class PasswordUtilTest {
 
     @Test
     public void verifyPassword() {
+        Properties properties = new Properties();
+        properties.setProperty("password.salt", "test");
+        PasswordUtil.setProperties(properties);
+
         User user = new User();
         // Hashed value of "foobar"+"test" (the salt value)
         user.setPassword("61e38e2b77827e10777ee8f1a138b7cfb1eb895");
         assertTrue(PasswordUtil.verifyPassword(user, "foobar"));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void getSaltSaltAlreadySet() throws SecurityException,
+            NoSuchFieldException, IllegalArgumentException,
+            IllegalAccessException, NoSuchMethodException,
+            InvocationTargetException {
+        Field field = PasswordUtil.class.getDeclaredField("salt");
+        field.setAccessible(true);
+        field.set(null, "foobar");
+
+        System.setProperty("authentication.password.salt", "test");
+        PasswordUtil.generateHashedPassword("test");
+    }
+
+    @Test
+    public void generateDefaultSalt() {
+        assertEquals("a4f1fbb1274f1fceba9dfae181d7afe6fca96f", PasswordUtil
+                .generateHashedPassword("foobar"));
     }
 
 }
